7d04a581e6929619398f2f59362fa30383b681e3
Cameron Wein <cameron.wein@gmail.com>
2021-11-24 14:02:58 -0600

    create employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,27 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Bob', 1, 'Bob.the.manager@gmail.com');
+    expect(employee.name).toBe('Bob');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('Bob.the.manager@gmail.com');
+});
+
+test("gets employee name", ()=> {
+    const employee = new Employee('Bob', 1, 'Bob.the.manager@gmail.com');
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+
+test("gets employee id", ()=> {
+    const employee = new Employee('Bob', 1, 'Bob.the.manager@gmail.com');
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
+
+test("gets employee name", ()=> {
+    const employee = new Employee('Bob', 1, 'Bob.the.manager@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+test("gets employee name", ()=> {
+    const employee = new Employee('Bob', 1, 'Bob.the.manager@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
